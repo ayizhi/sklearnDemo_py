d806f91c2b6549e67fa0919a5b723143ae59d7ee
yizhi <zhangyiizhi@126.com>
2016-10-12 23:13:32 +0800

    fix

--- linearRegression/linear.py
+++ linearRegression/linear.py
@@ -1,7 +1,18 @@
 #coding:utf-8
 import numpy as np
 import pandas as pd
+from pandas import DataFrame,Series
 from sklearn import linear_model
 
 #读取数据
+data = DataFrame(pd.read_csv('/Users/zhangyizhi/GitHub/sklearnDemo_py/data/data.csv'))
+data = data.dropna(how='all').fillna(0)
+data = data[data['getInMoney'] != 0]
 
+print data.columns
+print data
+print '========='
+print '========='
+print '========='
+print '========='
+# print data2
